22d940fb0d03e162f945b2b82d70b86858627bd4
55113678 <1970005177@qq.com>
2017-12-12 21:22:16 +0800

    WeiYing commit

--- WeiYing/app/src/main/java/com/example/administrator/weiying/activity/MainActivity.java
+++ WeiYing/app/src/main/java/com/example/administrator/weiying/activity/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.administrator.weiying.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.example.administrator.weiying.R;
 
@@ -11,5 +12,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.i("success","成功！");
     }
 }
