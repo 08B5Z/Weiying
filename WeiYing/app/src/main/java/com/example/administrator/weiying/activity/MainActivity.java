76b41a910b256a25dbe759a515efa3543365be96
55113678 <1970005177@qq.com>
2017-12-12 21:06:38 +0800

    commit weying

--- WeiYing/app/src/main/java/com/example/administrator/weiying/activity/MainActivity.java
+++ WeiYing/app/src/main/java/com/example/administrator/weiying/activity/MainActivity.java
@@ -1,7 +1,9 @@
-package com.example.administrator.weiying;
+package com.example.administrator.weiying.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.example.administrator.weiying.R;
 
 public class MainActivity extends AppCompatActivity {
 
