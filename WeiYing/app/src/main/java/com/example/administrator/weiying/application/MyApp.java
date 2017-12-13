d8d882e8ddac96cac38268f03da9df12c79bee04
55113678 <1970005177@qq.com>
2017-12-13 20:26:52 +0800

    commit Weiying

---
+++ WeiYing/app/src/main/java/com/example/administrator/weiying/application/MyApp.java
@@ -0,0 +1,14 @@
+package com.example.administrator.weiying.application;
+
+import android.app.Application;
+
+/**
+ * Created by Administrator on 2017/12/13.
+ */
+
+public class MyApp extends Application{
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
---
+++ WeiYing/app/src/main/java/com/example/administrator/weiying/net/Api.java
@@ -0,0 +1,8 @@
+package com.example.administrator.weiying.net;
+
+/**
+ * Created by Administrator on 2017/12/13.
+ */
+
+public class Api {
+}
