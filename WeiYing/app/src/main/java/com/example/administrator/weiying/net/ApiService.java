94996a34acec8aae89de5516fadb23e06265617b
unknown <lilin0719@163.com>
2017-12-15 16:20:22 +0800

    weiying

--- WeiYing/app/src/main/java/com/example/administrator/weiying/net/ApiService.java
+++ WeiYing/app/src/main/java/com/example/administrator/weiying/net/ApiService.java
@@ -1,9 +1,13 @@
 package com.example.administrator.weiying.net;
 
+import com.example.administrator.weiying.model.bean.NewsBean;
 import com.example.administrator.weiying.model.bean.ShouYeBean;
 
 import io.reactivex.Flowable;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.GET;
+import retrofit2.http.POST;
 
 /**
  * Created by Administrator on 2017/12/13.
@@ -13,4 +17,8 @@ public interface ApiService {
 
     @GET("front/homePageApi/homePage.do")
     Flowable<ShouYeBean> getHomePage();
+    //发现
+    @POST("front/columns/getVideoList.do")
+    @FormUrlEncoded
+    Flowable<NewsBean> getList(@Field("catalogId")String catalogId, @Field("pnum")String pnum);
 }
