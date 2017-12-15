fceb91d29c5647c2acaa94a7e88f9a252a7fb9ea
zpc <1378674684@qq.com>
2017-12-15 15:21:05 +0800

    weiying

--- WeiYing/app/src/main/java/com/example/administrator/weiying/activity/MainActivity.java
+++ WeiYing/app/src/main/java/com/example/administrator/weiying/activity/MainActivity.java
@@ -2,7 +2,14 @@ package com.example.administrator.weiying.activity;
 
 import android.graphics.Color;
 import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.administrator.weiying.R;
 import com.example.administrator.weiying.fragment.Fragment_book;
@@ -11,25 +18,92 @@ import com.example.administrator.weiying.fragment.Fragment_find;
 import com.example.administrator.weiying.fragment.Fragment_mine;
 import com.hjm.bottomtabbar.BottomTabBar;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private BottomTabBar btb;
+    private DrawerLayout dra;
+    private NavigationView nav;
+    private ImageView mUserIv;
+    private LinearLayout mShouc;
+    private LinearLayout mDown;
+    private LinearLayout mFuli;
+    private LinearLayout mSharee;
+    private LinearLayout mFank;
+    private LinearLayout mSeting;
+    /**
+     * 关于
+     */
+    private TextView mGuanyu;
+    /**
+     * 主题
+     */
+    private TextView mZhut;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        initView();
         btb = (BottomTabBar) findViewById(R.id.btb);
         btb.init(getSupportFragmentManager())
-                .setImgSize(28,28)
+                .setImgSize(28, 28)
                 .setFontSize(13)
-                .setTabPadding(4,6,10)
-                .setChangeColor(Color.RED,Color.DKGRAY)
-                .addTabItem("精选",R.mipmap.found_select,R.mipmap.found, Fragment_choice.class)
-                .addTabItem("专题",R.mipmap.special_select,R.mipmap.special,Fragment_book.class)
-                .addTabItem("发现",R.mipmap.fancy_select,R.mipmap.fancy, Fragment_find.class)
-                .addTabItem("我的",R.mipmap.my_select,R.mipmap.my, Fragment_mine.class)
+                .setTabPadding(4, 6, 10)
+                .setChangeColor(Color.RED, Color.DKGRAY)
+                .addTabItem("精选", R.mipmap.found_select, R.mipmap.found, Fragment_choice.class)
+                .addTabItem("专题", R.mipmap.special_select, R.mipmap.special, Fragment_book.class)
+                .addTabItem("发现", R.mipmap.fancy_select, R.mipmap.fancy, Fragment_find.class)
+                .addTabItem("我的", R.mipmap.my_select, R.mipmap.my, Fragment_mine.class)
                 .isShowDivider(false);
+    }
+
+    private void initView() {
+        mUserIv = (ImageView) findViewById(R.id.user_iv);
+        mUserIv.setOnClickListener(this);
+        mShouc = (LinearLayout) findViewById(R.id.shouc);
+        mShouc.setOnClickListener(this);
+        mDown = (LinearLayout) findViewById(R.id.down);
+        mDown.setOnClickListener(this);
+        mFuli = (LinearLayout) findViewById(R.id.fuli);
+        mFuli.setOnClickListener(this);
+        mSharee = (LinearLayout) findViewById(R.id.sharee);
+        mSharee.setOnClickListener(this);
+        mFank = (LinearLayout) findViewById(R.id.fank);
+        mFank.setOnClickListener(this);
+        mSeting = (LinearLayout) findViewById(R.id.seting);
+        mSeting.setOnClickListener(this);
+        mGuanyu = (TextView) findViewById(R.id.guanyu);
+        mGuanyu.setOnClickListener(this);
+        mZhut = (TextView) findViewById(R.id.zhut);
+        mZhut.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            default:
+                break;
+            case R.id.user_iv:
+               break;
+            case R.id.shouc:
+
+               break;
+            case R.id.down:
+                Toast.makeText(this, "敬请期待！", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.fuli:
+
+                break;
+            case R.id.sharee:
+                break;
+            case R.id.fank:
+                break;
+            case R.id.seting:
+                break;
+            case R.id.guanyu:
+                break;
+            case R.id.zhut:
+                break;
+        }
     }
 }
--- WeiYing/app/src/main/java/com/example/administrator/weiying/fragment/Fragment_mine.java
+++ WeiYing/app/src/main/java/com/example/administrator/weiying/fragment/Fragment_mine.java
@@ -6,6 +6,8 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import com.example.administrator.weiying.R;
 
@@ -13,12 +15,47 @@ import com.example.administrator.weiying.R;
  * Created by Administrator on 2017/12/13.
  */
 
-public class Fragment_mine extends Fragment {
+public class Fragment_mine extends Fragment implements View.OnClickListener {
+    private View view;
+    private LinearLayout mHistory;
+    private LinearLayout mCache;
+    private LinearLayout mShoucang;
+    private LinearLayout mZhuti;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment04, container, false);
 
+        initView(view);
         return view;
+    }
+
+    private void initView(View view) {
+        mHistory = (LinearLayout) view.findViewById(R.id.history);
+        mHistory.setOnClickListener(this);
+        mCache = (LinearLayout) view.findViewById(R.id.cache);
+        mCache.setOnClickListener(this);
+        mShoucang = (LinearLayout) view.findViewById(R.id.shoucang);
+        mShoucang.setOnClickListener(this);
+        mZhuti = (LinearLayout) view.findViewById(R.id.zhuti);
+        mZhuti.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            default:
+                break;
+            case R.id.history:
+               break;
+            case R.id.cache:
+                Toast.makeText(getActivity(), "敬请期待！", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.shoucang:
+                break;
+            case R.id.zhuti:
+                break;
+        }
     }
 }
